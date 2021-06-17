fe46ae8489077628acfe5ff5459b4fc1cd9f6d07
山崎睦美 <yamazakimutsumi@yamazakimutsuminoMacBook-Air.local>
2021-06-18 01:32:03 +0900

    Q8

--- task.php
+++ task.php
@@ -2,60 +2,132 @@
 # 回答の出力を確認される際は，「php task.php」をターミナルから実行して下さい。
 
 print("#####q1#####".PHP_EOL);
+
+<?php
 $names = ["田中", "佐藤", "佐々木", "高橋"];
 
   # 以下に回答を記載
 
+  array_push($names,"斉藤");
+  print_r($names);
+
+  
+
 echo PHP_EOL;
 
+?>
+
+
 print("#####q2#####".PHP_EOL);
+
+<?php
 $array1 = ["dog", "cat", "fish"];
 $array2 = ["bird", "bat", "tiger"];
 
   # 以下に回答を記載
 
+  $array = array_merge($array1,$array2);
+  print_r($array);
+
 echo PHP_EOL;
 
+?>
+
 print("#####q3#####".PHP_EOL);
+
+<?php
+
 $numbers = [1, 5, 8, 10, 2, 3, 2, 3, 1, 4, 5, 9];
 
   # 以下に回答を記載
+  $count = 0;
+  foreach($numbers as $number){
+     if($number === 3){
+        $count++;
+     }
+  }
+  print_r($count . "回".PHP_EOL);
+  
+  
 
 echo PHP_EOL;
 
+?>
+
 print("#####q4#####".PHP_EOL);
+
+<?php
 $sports = ["サッカー", "フットサル", null, "野球", "バスケ", null, "バレー"];
 
   # 以下に回答を記載
 
+  $sports = array_diff($sports,[null]);
+  print_r($sports);
+
+
 echo PHP_EOL;
 
+?>
+
 print("#####q5#####".PHP_EOL);
+
+<?php
+
 $array1 = [];
 $array2 = [1, 5, 8, 10];
 
   # 以下に回答を記載
 
+  $array1 = [];
+var_export(empty($array1));
+
+
+
 echo PHP_EOL;
 
-print("#####q6#####".PHP_EOL);
+?>
+
+print("#####q6#####".PHP_E
+
+<?php
 $numbers1 = [1, 2, 3, 4, 5];
 
   # 以下に回答を記載
 
+  $numbers2 = [];
+foreach($numbers1 as $number){
+    $number *= 10;
+    array_push($numbers2,$number);
+}
+print_r($numbers2);
+
 echo PHP_EOL;
 
+?>
+
 print("#####q7#####".PHP_EOL);
+
+<?php
+
 $array = ["1", "2", "3", "4", "5"];
 
   # 以下に回答を記載
+
+  $array = ["1", "2", "3", "4", "5"];
+$array = array_map('intval',$array);
+
 
   # 以下は変更しないで下さい
 var_dump($array);
 
 echo PHP_EOL;
 
+?>
+
 print("#####q8#####".PHP_EOL);
+
+<?php
+
 $programming_languages = ["php", "ruby", "python", "javascript"];
 
   # 以下に回答を記載
@@ -65,7 +137,15 @@ print_r($programming_languages);
 echo PHP_EOL;
 print_r($upper_case_programming_languages);
 
+print_r($programming_languages);
 echo PHP_EOL;
+print_r($upper_case_programming_languages);
+
+
+echo PHP_EOL;
+
+?>
+
 
 print("#####q9#####".PHP_EOL);
 $names = ["田中", "佐藤", "佐々木", "高橋"];
